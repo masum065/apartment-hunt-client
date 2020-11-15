a3f0b5a9f15b7d2cdb4995bfe4768b9530b03498
Masum065e <masumbillah065@gmail.com>
2020-11-15 19:50:31 +0600

    first commit for apartment hunt

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import './App.scss';
+
+function App() {
+	return (
+		<div className='App'>
+			<Container>
+				<Row className='justify-content-center'>
+					<Col xl={3}>
+						<h3>Hello React</h3>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+}
+
+export default App;
