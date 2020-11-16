c474fbfe019901c5ec80661bc280d9c624b7468f
motiur rahman <motiurrahman40507@gmail.com>
2020-11-16 20:46:50 +0600

    hello again done

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import Footer from '../../Shared/Footer/Footer';
->>>>>>> 7cf0663ce97604d7d1731dfc1eae7c130b1543b7
 import ApartmentList from '../ApartmentList/ApartmentList';
 import Header from '../Header/Header';
 import Service from '../Service/Service';
@@ -10,19 +7,12 @@ import './Home.scss';
 
 const Home = () => {
 	return (
-<<<<<<< HEAD
-		<>
-			<Header />
-			<ApartmentList />
-		</>
-=======
 		<div>
 			<Header />
 			<ApartmentList />
 			<Service />
 			<Footer />
 		</div>
->>>>>>> 7cf0663ce97604d7d1731dfc1eae7c130b1543b7
 	);
 };
 
